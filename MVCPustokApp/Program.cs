58f0454de8115c0bb64d4eeee744710efe14396d
Xalidaa <hacizadexalide@gmail.com>
2025-12-24 16:41:29 +0400

    admin task added

--- MVCPustokApp/Program.cs
+++ MVCPustokApp/Program.cs
@@ -9,7 +9,13 @@ namespace MVCPustokApp
             var app = builder.Build();
 
             app.UseStaticFiles();
-            
+
+            app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+
             app.MapControllerRoute
                 (
                 name: "default",
--- MVCPustokApp/obj/Debug/net8.0/MVCPustokApp.AssemblyInfo.cs
+++ MVCPustokApp/obj/Debug/net8.0/MVCPustokApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCPustokApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6abc16b042f3d7fc7d301341eec9eac9e634cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCPustokApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCPustokApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
